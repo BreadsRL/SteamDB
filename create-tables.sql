2317a5c4658f4914bc56d358076ce15b2727ea9f
Breads <72417429+BreadsRL@users.noreply.github.com>
2023-11-06 08:57:27 -0800

    Updated Schema table

--- create-tables.sql
+++ create-tables.sql
@@ -1,46 +1,67 @@
-CREATE TABLE Users(
-    u_Username char(12) NOT NULL,
-    u_Email varchar(320) NOT NULL,
-    u_Password varchar(256) NOT NULL
+-- Create the User table
+CREATE TABLE User (
+    Username TEXT PRIMARY KEY,
+    Email TEXT NOT NULL,
+    Password TEXT NOT NULL
 );
 
-CREATE TABLE Game(
-    g_GameID decimal(32,0) NOT NULL,
-    g_GameTitle varchar(256) NOT NULL,
-    g_ReleaseDate char(8) NOT NULL,
-    g_TagID char(4) NOT NULL,
-    g_PubID char(4) NOT NULL
+-- Create the Game table
+CREATE TABLE Game (
+    GameID INTEGER PRIMARY KEY,
+    GameTitle TEXT NOT NULL,
+    ReleaseDate DATE NOT NULL,
+    PublisherID INTEGER,
+    FOREIGN KEY (PublisherID) REFERENCES Publisher(PublisherID)
 );
 
-CREATE TABLE Publisher(
-    p_PubID char(4) NOT NULL,
-    p_Name varchar(256) NOT NULL
+-- Create the Tag table
+CREATE TABLE Tag (
+    TagID INTEGER PRIMARY KEY,
+    TagName TEXT NOT NULL
 );
 
-CREATE TABLE Tag(
-    t_TagID char(4) NOT NULL,
-    t_Name varchar(256) NOT NULL
+-- Create the GameTag table to manage the many-to-many relationship between Games and Tags
+CREATE TABLE GameTag (
+    GameID INTEGER,
+    TagID INTEGER,
+    PRIMARY KEY (GameID, TagID),
+    FOREIGN KEY (GameID) REFERENCES Game(GameID),
+    FOREIGN KEY (TagID) REFERENCES Tag(TagID)
 );
 
-CREATE TABLE Review(
-    r_ID char(10) NOT NULL,
-    r_Username char(12) NOT NULL,
-    r_GameID decimal(32,0) NOT NULL,
-    r_Rating char(8) NOT NULL,
-    r_Comment varchar(8000) NOT NULL,
-    r_Date char(8) NOT NULL
+-- Create the Publisher table
+CREATE TABLE Publisher (
+    PublisherID INTEGER PRIMARY KEY,
+    PublisherName TEXT NOT NULL
 );
 
-CREATE TABLE Sale(
-    s_ID char(10) NOT NULL,
-    s_StartDate char(8) NOT NULL,
-    s_EndDate char(8) NOT NULL,
-    s_GameID decimal(32,0) NOT NULL,
-    s_DiscountPercent varchar(3) NOT NULL
+-- Create the Wishlist table
+CREATE TABLE Wishlist (
+    WishlistID INTEGER PRIMARY KEY,
+    Username TEXT,
+    GameID INTEGER,
+    FOREIGN KEY (Username) REFERENCES User(Username),
+    FOREIGN KEY (GameID) REFERENCES Game(GameID)
 );
 
-CREATE TABLE Wishlist(
-    w_ID char(10) NOT NULL,
-    w_Username char(12) NOT NULL,
-    w_GameID decimal(32,0) NOT NULL
+-- Create the Review table
+CREATE TABLE Review (
+    ReviewID INTEGER PRIMARY KEY,
+    Username TEXT,
+    GameID INTEGER,
+    Rating INTEGER,
+    Comment TEXT,
+    ReviewDate DATE,
+    FOREIGN KEY (Username) REFERENCES User(Username),
+    FOREIGN KEY (GameID) REFERENCES Game(GameID)
+);
+
+-- Create the Sale table
+CREATE TABLE Sale (
+    SaleID INTEGER PRIMARY KEY,
+    Start_Date DATE,
+    End_Date DATE,
+    GameID INTEGER,
+    DiscountPercent REAL,
+    FOREIGN KEY (GameID) REFERENCES Game(GameID)
 );
