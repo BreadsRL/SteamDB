50283da3f4fb3c999a0cb5f684388254fb8789ff
Breads <72417429+BreadsRL@users.noreply.github.com>
2023-11-07 01:49:24 -0800

    Updated SQL statements

--- sql-statements.sql
+++ sql-statements.sql
@@ -9,22 +9,22 @@ INSERT INTO User (Username, Email, Password) VALUES
 
 -- 2. Adding Tags
 INSERT INTO Tag (TagName) VALUES
-    -- ('Combat'),
+    ('Test Tag');
     -- ('VR'),
     -- ('Story Rich'),
     -- ('Adventure');
 
 -- 3. Adding Publishers
 INSERT INTO Publisher (PublisherName) VALUES
-    -- ('Gaijin Distribution KFT');
+    ('Test Company');
 
 -- 4. Adding Games
 INSERT INTO Game (GameTitle, ReleaseDate, PublisherID) VALUES
-    -- ('War Thunder', '2013-08-15', 8);
+    ('Test Game', '2013-08-15', 9);
 
 -- 5. Adding Game-Tag relations
 INSERT INTO GameTag (GameID, TagID) VALUES
-    -- (5, 10),
+    (11, 25);
     -- (5, 2),
     -- (5, 12),
     -- (5, 9);
@@ -33,7 +33,7 @@ INSERT INTO GameTag (GameID, TagID) VALUES
 
 -- 6. Adding Wishlists
 INSERT INTO Wishlist (Username, GameID) VALUES
-    ('user7', 10);
+    ('user7', 11);
     -- ('user6', 6),
     -- ('user6', 7),
     -- ('user6', 8),
@@ -41,11 +41,11 @@ INSERT INTO Wishlist (Username, GameID) VALUES
 
 -- 7. Adding reviews, 0 for positive rating, 1 for negative rating
 INSERT INTO Review (Username, GameID, Rating, Comment, ReviewDate) VALUES
-    ('user7', 10, 1, 'Test', '2016-02-15');
+    ('user7', 11, 1, 'Test', '2016-02-15');
 
 -- 8. Adding Games for sale
 INSERT INTO Sale (Start_Date, End_Date, GameID, DiscountPercent) VALUES
-    -- ('2023-10-05', '2023-11-01', 6, 51.1),
+    ('2023-10-05', '2023-11-01', 11, 54.25);
     -- ('2023-11-10', '2023-11-18', 9, 15);
     
 -- 9. Search for a game by one tag
@@ -53,7 +53,7 @@ SELECT DISTINCT Game.GameTitle
 FROM Game
 JOIN GameTag ON Game.GameID = GameTag.GameID
 JOIN Tag ON GameTag.TagID = Tag.TagID
-WHERE Tag.TagName = 'Action' OR Tag.TagName = 'FPS';
+WHERE Tag.TagName = 'Action' OR Tag.TagName = 'Test Tag';
 
 -- 10. Search for a game by multiple tags
 SELECT DISTINCT Game.GameTitle
@@ -67,15 +67,15 @@ HAVING COUNT(DISTINCT Tag.TagID) = 2;
 -- 11. Edit a review for a certain user and game
 UPDATE Review
 SET Rating = 1, Comment = 'Update test', ReviewDate = '2023-11-05'
-WHERE Username = 'user6' AND GameID = 10;
+WHERE Username = 'user7' AND GameID = 11;
 
 -- 12. Delete reviews for a certain user and game
 DELETE FROM Review
-WHERE Username = 'user6' AND GameID = 10;
+WHERE Username = 'user7' AND GameID = 11;
 
 -- 13. Remove a game from a user's wishlist
 DELETE FROM Wishlist
-WHERE Username = 'user6' AND GameID = 10;
+WHERE Username = 'user7' AND GameID = 11;
 
 -- 14. Search for a game by title and see its details
 SELECT Game.GameTitle, Game.ReleaseDate, Publisher.PublisherName, GROUP_CONCAT(DISTINCT Tag.TagName) AS Tags
@@ -83,7 +83,7 @@ FROM Game
 LEFT JOIN Publisher ON Game.PublisherID = Publisher.PublisherID
 LEFT JOIN GameTag ON Game.GameID = GameTag.GameID
 LEFT JOIN Tag ON GameTag.TagID = Tag.TagID
-WHERE Game.GameTitle = 'Counter-Strike 2';
+WHERE Game.GameTitle = 'Test Game';
 
 -- 15. View current ongoing game sales
 SELECT Game.GameTitle, Sale.Start_Date, Sale.End_Date, Sale.DiscountPercent
@@ -98,24 +98,32 @@ INNER JOIN Sale ON Game.GameID = Sale.GameID
 WHERE date('now') <= Sale.End_Date;
 
 -- 17. Delete a user's account and related records
-DELETE FROM Wishlist WHERE Username = 'user7';
-DELETE FROM Review WHERE Username = 'user7';
-DELETE FROM User WHERE Username = 'user7';
+DELETE FROM Wishlist WHERE Username = 'user2';
+DELETE FROM Review WHERE Username = 'user2';
+DELETE FROM User WHERE Username = 'user2';
 
 -- 18. Update the user's username
 UPDATE User
-SET Username = 'new_username'
-WHERE Username = 'current_username';
+SET Username = 'user65'
+WHERE Username = 'user3';
+
+UPDATE Wishlist
+SET Username = 'user65'
+WHERE Username = 'user3';
+
+UPDATE Review
+SET Username = 'user65'
+WHERE Username = 'user3';
 
 -- 19. Update the user's email
 UPDATE User
-SET Email = 'new_email'
-WHERE Username = 'current_username';
+SET Email = 'user65@email.com'
+WHERE Username = 'user65';
 
 -- 20. Update the user's password
 UPDATE User
-SET Password = 'new_password'
-WHERE Username = 'current_username'
+SET Password = 'password65'
+WHERE Username = 'user65';
 
 -- Delete Table values
 DELETE from Game;
